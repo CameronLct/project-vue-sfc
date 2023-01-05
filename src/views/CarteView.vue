0db876f3fbbb35d463191a9ade413601536ad960
txomac <thomas.hnf@gmail.com>
2023-01-05 15:39:42 +0100

    add leaflet map

--- src/views/CarteView.vue
+++ src/views/CarteView.vue
@@ -1,5 +1,38 @@
 <template>
   <div class="carte">
-    <h1>This is an carte page</h1>
+    <div style="height:600px; width:800px">
+      <l-map ref="map" v-model:zoom="zoom" :center="[47.41322, -1.219482]">
+        <l-tile-layer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            layer-type="base"
+            name="OpenStreetMap"
+        ></l-tile-layer>
+      </l-map>
+    </div>
   </div>
 </template>
+
+
+<script>
+import "leaflet/dist/leaflet.css";
+import { LMap, LTileLayer } from "@vue-leaflet/vue-leaflet";
+
+export default {
+  components: {
+    LMap,
+    LTileLayer,
+  },
+  data() {
+    return {
+      zoom: 5,
+    };
+  },
+};
+</script>
+
+<style>
+  #map {
+    height: 180px;
+    width: 180px;
+}
+</style>
